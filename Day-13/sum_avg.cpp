d28cf2f6915541a8efb61b4bc87d1fa4783112a4
Laksh Sharma <laksh1441942@gmail.com>
2026-06-13 15:42:18 +0530

    Today's programs

---
+++ Day-13/sum_avg.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    int sum = 0;
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    double average = (double)sum / n;
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average << endl;
+
+    return 0;
+}
